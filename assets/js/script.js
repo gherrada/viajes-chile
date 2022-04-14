24289f65c84e4808e1c8b20aa2a32f5bb2d1d0d7
gherrada <gonzalo.herrada@gmail.com>
2022-04-13 21:57:24 -0400

    smooth scroll agregado

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,18 @@ $(document).ready(function(){
     var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl);
     });
-
-})
+    
+    $("a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+        
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                window.location.hash = hash;
+            });
+        }
+    });
+});
 
